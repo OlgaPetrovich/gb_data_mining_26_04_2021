8b44dd1e08502b49d17b174012dbc555a029a11b
Olya <olgabelomittseva@gmail.com>
2021-05-17 19:22:51 +0300

    les5+HW5

--- gb_parse/loaders.py
+++ gb_parse/loaders.py
@@ -10,9 +10,25 @@ def clear_price():
     return result
 
 
+def flat_text(items):
+    return "\n".join(items)
+
+
+def hh_user_url(user_id):
+    return urljoin("https://hh.ru/", user_id)
+
 class AutoyoulaLoader(ItemLoader):
     default_item_class = dict
     url_out = TakeFirst()
     title_out = TakeFirst()
     price_in = MapCompose(clear_price)
     price_out = TakeFirst()
+
+
+class HHLoader(ItemLoader):
+    default_item_class = dict
+    url_out = TakeFirst()
+    title_out = TakeFirst()
+    salary_out = flat_text
+    author_in = MapCompose(hh_user_url)
+    author_out = TakeFirst()
